FIX: loss calculation

transformer/generate.py
@@ -37,7 +37,6 @@ def generate_step(model, inputs, length, key, temperature):
     logits = jnp.take(logits, length - 1, axis=0)
 
     if temperature == 0.0:
-        # argmax sampling
         return jnp.argmax(logits, axis=-1)
 
     logits = logits / temperature
@@ -79,8 +78,6 @@ def generate_loop(
     )
     input_ids = np.pad(initial_input, ((0, input_length - sample_idx),))
 
-    # TODO: maybe replace with jax.lax.scan loop for faster generation
-    # https://stackoverflow.com/questions/77364000/convert-for-loop-to-jax-lax-scan
     for i in tqdm.trange(max_to_generate, desc="Generating"):
         key, subkey = jax.random.split(key)
         inputs = dict(input_ids=input_ids, position_ids=position_ids, mask=mask)
@@ -149,8 +146,6 @@ def main(args):
             "If you do not intend to use random weights, please specifiy --checkpoint when excecuting script."
         )
     else:
-        # this does not restore things like activation functions and such, so
-        # we need to use a tree map later to recover these details.
         logger.info(f"Loading model from '{Path(PurePath(args.checkpoint_directory).name) / args.checkpoint}'")
         checkpointer = ocp.Checkpointer(ocp.StandardCheckpointHandler())
         loaded_model = checkpointer.restore(
@@ -158,8 +153,6 @@ def main(args):
             item=eqx.filter([model], eqx.is_inexact_array),
         )[0]
 
-        # hack to deal with optax not serialising some equinox hyperparameters
-        # TODO: change to use eqx serialisation to avoid this.
         model = jax.tree_map(lambda x, y: x if (y is None) else y, model, loaded_model)
         del loaded_model
 
@@ -169,7 +162,7 @@ def main(args):
     logger.info(f"Model has {num_parameters:,} parameters.")
 
     if args.prompt_mode == "unconditional":
-        start_tokens = np.array([1], dtype=int)  # BOS token only
+        start_tokens = np.array([1], dtype=int)
     elif args.prompt_mode == "file":
         logger.info(f"Loading prompt file '{args.prompt_midi}'")
         midi = file_prompt(args.prompt_midi)

transformer/model/attention.py
@@ -1,4 +1,3 @@
-# copied from Equinox library to add softmax dtype
 import functools as ft
 import math
 import warnings
@@ -30,7 +29,7 @@ def dot_product_attention_weights(
             )
         logits = jnp.where(mask, logits, jnp.finfo(logits.dtype).min)
 
-    return jax.nn.softmax(logits, axis=-1)  # pyright: ignore
+    return jax.nn.softmax(logits, axis=-1)
 
 
 def dot_product_attention(
@@ -146,7 +145,7 @@ class MultiheadAttention(Module):
         use_output_bias: bool = False,
         dropout_p: float = 0.0,
         inference: bool = False,
-        dtype: jnp.dtype = jnp.float32,  # softmax is always fixed to f32
+        dtype: jnp.dtype = jnp.float32,
         *,
         key: PRNGKeyArray,
         **kwargs,
@@ -264,7 +263,6 @@ class MultiheadAttention(Module):
         kv_seq_length, _ = key_.shape
         kv_seq_length2, _ = value.shape
         if kv_seq_length != kv_seq_length2:
-            # query length can be different
             raise ValueError("key and value must both be sequences of equal length.")
 
         # query, key_, value = query.astype(self.dtype), key_.astype(self.dtype), value.astype(self.dtype)
@@ -281,12 +279,10 @@ class MultiheadAttention(Module):
         )
         keys = None if key is None else jax.random.split(key, query_heads.shape[1])
         if mask is not None and mask.ndim == 3:
-            # Batch `mask` and `keys` down their 0-th dimension.
             attn = jax.vmap(attn_fn, in_axes=1, out_axes=1)(
                 query_heads, key_heads, value_heads, mask=mask, key=keys
             )
         else:
-            # Batch `keys` down its 0-th dimension.
             attn = jax.vmap(ft.partial(attn_fn, mask=mask), in_axes=1, out_axes=1)(
                 query_heads, key_heads, value_heads, key=keys
             )

transformer/train.py
@@ -74,8 +74,6 @@ def create_train_step(model, optimiser):
     # @eqx.debug.assert_max_traces(max_traces=1)
     @eqx.filter_jit
     def train_step(model, opt_state, batch, key):
-        # TODO: some of these arguments are different between first and second step
-        # need to investigate to avoid a double compile.
         batch, labels, keys = prepare_batch(batch, key)
         (loss, _), grads = eqx.filter_value_and_grad(loss_fn, has_aux=True)(model, batch, labels, keys)
 
@@ -157,7 +155,6 @@ def main(args):
     logger.info(f"Model has {num_parameters:,} parameters.")
 
     if args.use_bf16:
-        # map all params to bf16
         logger.info("Training with bfloat16.")
         model = jax.tree_util.tree_map(lambda p: p.astype(jnp.bfloat16) if eqx.is_inexact_array(p) else p, model)
 
@@ -180,7 +177,7 @@ def main(args):
         drop_last=True,
     )
 
-    check that the dataloader is working
+    #check that the dataloader is working
     for i, batch in enumerate(train_loader):
         print(batch)
         if i > 5: